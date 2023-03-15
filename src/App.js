41f53990395aefa40636ad7e28825e3c4802536b
Jnava1342 <Jpeace1342@hotmail.com>
2023-03-15 09:39:16 -0500

    udated app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>
+  return (
+  <div className="App">
+    <h1>Weather App</h1>
+
+
+
+
+
+    </div>
+  );
 }
 
 
