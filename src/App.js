a78bb50f9324e97f3779ba187b07dccb5f524f85
Jnava1342 <Jpeace1342@hotmail.com>
2023-03-15 10:02:13 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 export default function App() {
   return (
   <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
-
-
-
-
-
+    <footer>
+      This project was coded by {""}
+      <a href="https://github.com/Jpeace1342/react-weather-appp"target="_blank">
+        Jamie Navarro 
+      </a>{""} 
+      and is{""}
+      <a href="https://github.com/Jpeace1342/react-weather-appp"target="_blank">
+    open sourced on GitHub
+    </a>
+    </footer>
+    </div>
     </div>
   );
 }
