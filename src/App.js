e1fcfe8af8abe5970a02bc2838f8a0f7e1167337
Jnava1342 <Jpeace1342@hotmail.com>
2023-03-19 06:39:20 -0500

    added css and axios to terminal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import './App.css';
-
+import Weather from "./Weather";
 export default function App() {
   return (
   <div className="App">
     <div className="container">
-    <h1>Weather App</h1>
+    <Weather />
+
+
     <footer>
       This project was coded by {""}
-      <a href="https://github.com/Jpeace1342/react-weather-appp"target="_blank">
+      <a href="https://github.com/Jpeace1342/react-weather-appp"rel="noreferrer"target="_blank">
         Jamie Navarro 
       </a>{""} 
       and is{""}
-      <a href="https://github.com/Jpeace1342/react-weather-appp"target="_blank">
+      <a href="https://github.com/Jpeace1342/react-weather-appp"rel="noreferrer"target="_blank">
     open sourced on GitHub
     </a>
     </footer>
