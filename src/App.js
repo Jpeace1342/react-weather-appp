fcac1626f7db1c6a3275644f0fd315c31213b671
Jnava1342 <Jpeace1342@hotmail.com>
2023-03-26 13:11:24 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ export default function App() {
   return (
   <div className="App">
     <div className="container">
-    <Weather />
+     <Weather defaultCity="New York" />
 
 
     <footer>
-      This project was coded by {""}
+      This project was coded by {" "}
       <a href="https://github.com/Jpeace1342/react-weather-appp"rel="noreferrer"target="_blank">
         Jamie Navarro 
-      </a>{""} 
-      and is{""}
+      </a>{" "} 
+      and is{" "}
       <a href="https://github.com/Jpeace1342/react-weather-appp"rel="noreferrer"target="_blank">
     open sourced on GitHub
     </a>
--- src/Weather.js
+++ src/Weather.js
@@ -1,78 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 
-export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [weatherData, setWeatherData] = useState({});
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
-      wind: 12,
-      city: response.data.name
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
     });
-
-    setReady(true);
   }
-  if (ready) {
-  return(
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input 
-            type="search"
-            placeholder="Enter a city.."
-            className="form-control"
-            autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary w-100" />
-          </div>
-        </div>
-      </form>
-      
-      <h1>{weatherData.city}</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>{weatherData.description}</li>
-      </ul>
 
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-         
-           <div className="float-left">
-          <span className="temperature">{Math.round (weatherData.temperature)}</span>
-          <span className="unit">°C</span>
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-          <img className="float-left"
-          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-          alt="Mostly Cloudy"/>
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apikey = "42ba0b3b90cb71o34dfbt8899738ed50";
+    let city = "New York";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+
+    axios.get(apiURL).then(handleResponse);
+    }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-        </div>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 15%</li>
-              <li>Humidity: 72%</li>
-              <li>Wind:{weatherData.wind} Km/h</li>
-            </ul>
-            
-        </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
-    </div>
-  );
-} else {
- const apikey = "42ba0b3b90cb71o34dfbt8899738ed50";
-  let city = "New York";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&appid=${apikey}&units=metric`;
-  axios.get(apiURL).then(handleResponse);
-
-  return "Loading...";
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
-}
- 
